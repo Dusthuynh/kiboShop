27cd89a19e5410e97533860bcce9383056a56bcc
Huynh Dust <huynhdust0411@gmail.com>
2021-10-15 18:02:41 +0700

    capnhat padding header

--- headAndFooter/hf.js
+++ headAndFooter/hf.js
@@ -7,12 +7,12 @@ fetch("/headAndFooter/header.html")
   });
 
 fetch("/headAndFooter/footer.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("footer").innerHTML = data;
-  });
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.querySelector("footer").innerHTML = data;
+    });
 
   if(!document.getElementById('hf')) {
     var link = document.createElement('link');
