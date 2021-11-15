a205f81ff9fe284685cd8b2075d2d572ecbfbb27
Tarykege <xoiiwkingg@gmail.com>
2021-11-15 19:34:36 +0700

    update cart

--- gio-hang/cart.js
+++ gio-hang/cart.js
@@ -1,16 +1,18 @@
+var DataSet = data.Akko.concat(data.Leopold.concat(data.Keychron));
 window.onload = main();
 
 function main() {
 
-    var testObject = { 'one': 1, 'two': 2, 'three': 3 };
+    // var testObject = { 'one': 1, 'two': 2, 'three': 3 };
 
-    // Put the object into storage
-    localStorage.setItem('testObject', JSON.stringify(testObject));
+    // // Put the object into storage
+    // localStorage.setItem('testObject', JSON.stringify(testObject));
 
-    // Retrieve the object from storage
-    var retrievedObject = localStorage.getItem('testObject');
+    // // Retrieve the object from storage
+    // var retrievedObject = localStorage.getItem('testObject');
 
-    console.log('retrievedObject: ', JSON.parse(retrievedObject));
+    // console.log('retrievedObject: ', JSON.parse(retrievedObject));
+    
 }
 // Lấy data set tất cả sản phẩm
 // Lấy dữ liệu từ local Storage
